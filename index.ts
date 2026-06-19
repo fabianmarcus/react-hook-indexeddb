2864d56a3d269029c8f79d15ab59e2729a750552
Fabian Marcus <fmarcus@conet.de>
2026-06-19 14:04:28 +0200

    Refactor type exports in index.ts for better organization

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
-export type { IndexedDbIndex } from "./src/types/IndexedDbIndex";
-export type { HookProps, UseIndexedDb } from "./src/hook.indexeddb";
 export { generateLocalId, isLocalId, useIndexedDb } from "./src/hook.indexeddb";
+
+export type {
+  HookProps,
+  IndexedDbIndex,
+  UseIndexedDb,
+} from "./src/hook.indexeddb";
